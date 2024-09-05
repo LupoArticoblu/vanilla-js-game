95b8ad6497e58ad5a2477c08117907981f51c109
LupoArticoblu <wolf94-@libero.it>
2024-09-05 13:19:38 +0200

    suddivisione di un frame e relativa velocità di riproduzione

--- script.js
+++ script.js
@@ -19,22 +19,58 @@ const spriteHeight = 523; //<- questo valore è il risultato della divisione tra
 let frameX = 0;
 let frameY = 0;
 
+let gameFrame = 0;
+//impostiamo un valore per l'accellerazione dei frames
+const stuggleFrame = 5;
+
 //animiamo il nostro personaggio
 function animate(){
  
   //clear rectangle = cancella l'animazione precedente,in parentesi si aspetta quale area del canvas ripulire tramite cordinate
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  //disegnamo il nostro personaggio
+  /*vediamo la nostra prima animazione all'opera
+  
+  if(frameX < 6) frameX++;
+  else frameX = 0;
+
+  la prima difficoltà sarà nel vedere come diverse animazioni hanno anche quantità di frame diversi
+  
+  ecco come suddividere i frame in base alla quantità di frame
+  gameframe/stuggleframe = ???
+  0/5 = 0 -> Math.floor(0) = 0
+  1/5 = 0.2 -> Math.floor(0.2) = 0 => 0 % 6 = 0
+  2/5 = 0.4 -> Math.floor(0.4) = 0 => ""
+  3/5 = 0.6 -> Math.floor(0.6) = 0 => ""
+  4/5 = 0.8 -> Math.floor(0.8) = 0 => ""
+  5/5 = 1 -> Math.floor(1) = 1 => 1 % 6 = 1
+  6/5 = 1.2 -> Math.floor(1.2) = 1 => ""
+  7/5 = 1.4 -> Math.floor(1.4) = 1 => ""
+  8/5 = 1.6 -> Math.floor(1.6) = 1 => ""
+  9/5 = 1.8 -> Math.floor(1.8) = 1 => ""
+  10/5 = 2 -> Math.floor(2) = 2 => 2 % 6 = 2 ecc...
+  */
+
+  let position = Math.floor(gameFrame / stuggleFrame) % 6;
+  
+  //reimpostiamo il valore di frameX
+  frameX = spriteWidth * position;
+
+    //disegnamo il nostro personaggio
   
   //prima prendiamo le dimensioni del personaggio sul layer del canvas,poi le assegnamo all'immagine, s=source, d=destination ossia la parte "croppata"
   
   //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh); prendiamo solo sx, sy, sw, sh saranno le dimensioni interessate, e le inseriamo dopo il valore playerImage di drawImage
   
   //sostituendo i valori di source x e y con delle ripetizioni di spriteWidth e spriteHeight, otteremo i frame della nostra immagine in movimento. Esempio frame 1(sx = 0 * spriteWidth), frame 2(sx = 1 * spriteWidth) e cosi via. mentre sy ci permetterà di spostarci da una posa ad un'altra, esempio stand 1(sy = 0 * spriteHeight), jump 1(sy = 1 * spriteHeight), land 1(sy = 2 * spriteHeight) ecc...   
-  ctx.drawImage(playerImage, frameX * spriteWidth, frameY *  spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(playerImage, frameX, frameY *  spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-
+  //utilizziamo gameFrame ed usiamo il modulo per farlo ciclare
+  if(gameFrame % stuggleFrame === 0) {
+    if(frameX < 8) frameX++;
+    else frameX = 0;
+  }
+  gameFrame++;
   //per un loop di animazioni
   requestAnimationFrame(animate);
 }
